d4b52102c106b1d73a4298c74f911436b3b2b633
juhi-coder <kumarijuhimdb2@gmail.com>
2023-04-01 20:54:29 +0530

    Adding a Form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { Fragment } from "react";
 import Header from "./components/Header/Header";
-import MealsSummary from './components/Meals/MealsSummary'
 import Meals from "./components/Meals/Meals";
-import MealItem from "./components/Meals/MealItem";
 function App() {
   return (
     <Fragment>
--- src/components/Cart/CartIcon.js
+++ src/components/Cart/CartIcon.js
@@ -2,9 +2,13 @@ const CartIcon=()=>{
 
     return (
             
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-    </svg>
+            <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
+          </svg>
     )
 }
 export default CartIcon;
