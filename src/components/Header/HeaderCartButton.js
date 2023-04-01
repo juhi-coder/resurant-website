65da8d1b4ead3127d1dfdd1ff5cbee95f801238f
juhi-coder <kumarijuhimdb2@gmail.com>
2023-04-01 10:13:04 +0530

    resurant-website-header

---
+++ src/components/Header/HeaderCartButton.js
@@ -0,0 +1,12 @@
+import CartIcon from "../Cart/CartIcon";
+import classes from'./HeaderCartButton.module.css';
+
+const HeaderCartButton=(props)=>{
+return <button className={classes.button}>
+    <span className={classes.icon} ><CartIcon></CartIcon></span>
+<span>Your Cart</span>
+<spam className={classes.badge}>0</spam>
+</button>
+
+}
+export default HeaderCartButton;
